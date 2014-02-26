d4458a9eb3ecceae09dcfc701b7438c5b44de9d6
Mogens Heller Grabe <mookid8000@gmail.com>
2014-02-26 23:19:04 +0100

    added everything else

---
+++ RobotsOnFire/Installers/TestInstaller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Timers;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using NLog;
+
+namespace RobotsOnFire.Installers
+{
+    public class TestInstaller : IWindsorInstaller
+    {
+        static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<IDisposable>()
+                    .Instance(CreateAndStartTestTimer(interval: TimeSpan.FromSeconds(1)))
+                    .Named("TestTimer")
+                );
+        }
+
+        Timer CreateAndStartTestTimer(TimeSpan interval)
+        {
+            var timer = new Timer(interval.TotalMilliseconds);
+            timer.Elapsed += (o, ea) => Log.Info("woooH0000! It's going great!!1");
+            timer.Start();
+            return timer;
+        }
+    }
+}
---
+++ RobotsOnFire/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using NLog;
+using Topshelf;
+
+namespace RobotsOnFire
+{
+    class Program : ServiceControl
+    {
+        static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        static void Main()
+        {
+            HostFactory.Run(f =>
+            {
+                f.UseNLog();
+                f.Service<Program>();
+            });
+        }
+
+        readonly WindsorContainer _container = new WindsorContainer();
+
+        public bool Start(HostControl hostControl)
+        {
+            try
+            {
+                Log.Info("Initializing container");
+                _container.Install(FromAssembly.This());
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Log.Error("An error occurred during intialization: {0}", exception);
+
+                return false;
+            }
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            try
+            {
+                Log.Info("Disposing container");
+                _container.Dispose();
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Log.Error("An error occurred during container disposal: {0}", exception);
+
+                return false;
+            }
+        }
+    }
+}
