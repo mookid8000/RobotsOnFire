896375d1867255ca61f37a8dd480931a7180c85e
Mogens Heller Grabe <mookid8000@gmail.com>
2014-02-26 23:24:12 +0100

    moved service into own class

--- RobotsOnFire/Program.cs
+++ RobotsOnFire/Program.cs
@@ -1,58 +1,22 @@
 ﻿using System;
-using Castle.Windsor;
-using Castle.Windsor.Installer;
 using NLog;
 using Topshelf;
 
 namespace RobotsOnFire
 {
-    class Program : ServiceControl
+    class Program
     {
         static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
         static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += (o, ea) => Log.Fatal("Unhandled exception: {0}", ea.ExceptionObject);
+
             HostFactory.Run(f =>
             {
                 f.UseNLog();
-                f.Service<Program>();
+                f.Service<RobotService>();
             });
-        }
-
-        readonly WindsorContainer _container = new WindsorContainer();
-
-        public bool Start(HostControl hostControl)
-        {
-            try
-            {
-                Log.Info("Initializing container");
-                _container.Install(FromAssembly.This());
-
-                return true;
-            }
-            catch (Exception exception)
-            {
-                Log.Error("An error occurred during intialization: {0}", exception);
-
-                return false;
-            }
-        }
-
-        public bool Stop(HostControl hostControl)
-        {
-            try
-            {
-                Log.Info("Disposing container");
-                _container.Dispose();
-
-                return true;
-            }
-            catch (Exception exception)
-            {
-                Log.Error("An error occurred during container disposal: {0}", exception);
-
-                return false;
-            }
         }
     }
 }
---
+++ RobotsOnFire/RobotService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using NLog;
+using Topshelf;
+
+namespace RobotsOnFire
+{
+    class RobotService : ServiceControl
+    {
+        static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        readonly WindsorContainer _container = new WindsorContainer();
+
+        public bool Start(HostControl hostControl)
+        {
+            try
+            {
+                Log.Info("Initializing container");
+
+                _container.Install(FromAssembly.This());
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Log.Error("An error occurred during intialization: {0}", exception);
+
+                return false;
+            }
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            try
+            {
+                Log.Info("Disposing container");
+
+                _container.Dispose();
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Log.Error("An error occurred during container disposal: {0}", exception);
+
+                return false;
+            }
+        }
+    }
+}
